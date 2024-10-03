abbec3b1aab2afef62c6546b35ad3c3760472266
Abdulaziz <94856585+AbdulazizT-IT@users.noreply.github.com>
2024-10-03 23:44:50 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -24,3 +24,13 @@ def power(x, y):
 
 def sqrt(x):
     pass
+
+# For the add() function
+$ py.test test_add.py
+
+# For the subtract() function
+$ py.test test_subtract.py
+
+# For the mulitply() function
+$ py.test test_multiply.py
+
